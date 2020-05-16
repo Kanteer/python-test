7ad00bc28c4105a06b798e1c45f88da3d41de031
kanteer <59344076+Kanteer@users.noreply.github.com>
2020-05-15 21:32:36 -0400

    Update bbrruuhh.py

--- bbrruuhh.py
+++ bbrruuhh.py
@@ -1,11 +1,13 @@
 import time
 
-print("is working?")
+print("is_working")
 time.sleep(0.4)
-print("true")
+print("True")
 time.sleep(0.6)
 print("closing in 3")
 time.sleep(1)
 print("2")
 time.sleep(1)
 print("1")
+time.sleep(3)
+print("bitch you thought")
